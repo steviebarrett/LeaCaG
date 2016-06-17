dc99a82df6f5004881747be83a9506b56c5eed48
conmaol <Mark.McConville@glasgow.ac.uk>
2016-06-17 22:16:23 +0100

    Edited php files to process gaelic index.

--- api/ajax.php
+++ api/ajax.php
@@ -7,7 +7,12 @@ switch ($action) {
 	case "getEnglish":
 		$query = $_GET["q"];
 		echo getEnglish($query);
-		break; 
+		break;
+
+	case "getGaelic":
+		$query = $_GET["q"];
+		echo getGaelic($query);
+		break;
 }
 
 function getEnglish($q) {
@@ -16,6 +21,18 @@ function getEnglish($q) {
 	$json = json_decode($englishIndex, true);
 	foreach ($json["english_index"] as $item) {
 		if (strtolower(substr($item["en"], 0, strlen($q))) == strtolower($q)) {
+			$results[] = $item;
+		}
+	}
+	return json_encode(array("results"=>$results));
+}
+
+function getGaelic($q) {
+	$gaelicIndex = file_get_contents("../../lexicopia/gd/target-index.json");
+	$results = array();
+	$json = json_decode($gaelicIndex, true);
+	foreach ($json["gaelic_index"] as $item) {
+		if (strtolower(substr($item["word"], 0, strlen($q))) == strtolower($q)) {
 			$results[] = $item;
 		}
 	}
